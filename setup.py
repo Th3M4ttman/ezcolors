9697f74e38ccb11b081b796f789b7f002a6ebb61
Th3M4ttman <WorkMatthewJHarris94@gmail.com>
2021-11-06 01:43:08 +0000

    1.0.2
    Fully fixed all configs and published to pypi

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="ezcolors",
-    version="1.0.1",
+    version="1.0.2",
     description="Easy colors and color math",
     long_description=README,
     long_description_content_type="text/markdown",
